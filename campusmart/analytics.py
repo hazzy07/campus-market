ebbf4b3254fbd4828770cbcdbd9ed4b4a64722dd
hazzy07 <hafizalihamza047@gmail.com>
2026-08-19 00:08:44 +0500

    working on the part c

---
+++ campusmart/analytics.py
@@ -0,0 +1,22 @@
+def revenue_by_category(products, sales):
+    return {
+        
+    }
+
+def low_stocks(products):
+    return (
+
+    )
+
+def repeat_customers(sales):
+    return {
+
+    }
+
+def price_list(products):
+    return list(
+
+    )
+
+def top_n(pairs, n):
+    return()
